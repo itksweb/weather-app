85396517dc68c88087cc5ec7cb82e20358dc2435
itksweb <itksweb@gmail.com>
2025-10-31 11:36:11 +0100

    conxtext provided

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { WeatherInfoProvider } from './store/weatherInfoContext.js'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <WeatherInfoProvider>
+      <App />
+    </WeatherInfoProvider>
+  </StrictMode>
+);
